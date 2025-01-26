ce8aa5f05de7b1cebd6486158aaa94e0e02168cf
Yer01 <enunaaaa.zhekenov@gmail.com>
2025-01-26 21:55:52 +0500

    Interfaces

--- sample-app/main_test.go
+++ sample-app/main_test.go
@@ -3,35 +3,21 @@ package main
 import (
 	"fmt"
 	"testing"
-	"time"
 )
 
 func Test(t *testing.T) {
 	type testCase struct {
-		msg          message
-		expectedText string
-		expectedCost int
+		emp      employee
+		expected int
 	}
 	tests := []testCase{
-		{birthdayMessage{time.Date(1994, 03, 21, 0, 0, 0, 0, time.UTC), "John Doe"},
-			"Hi John Doe, it is your birthday on 1994-03-21T00:00:00Z",
-			168,
-		},
-		{sendingReport{"First Report", 10},
-			`Your "First Report" report is ready. You've sent 10 messages.`,
-			183,
-		},
+		{fullTime{name: "Bob", salary: 7300}, 7300},
+		{contractor{name: "Jill", hourlyPay: 872, hoursPerYear: 982}, 856304},
 	}
 	if withSubmit {
 		tests = append(tests, []testCase{
-			{birthdayMessage{time.Date(1934, 05, 01, 0, 0, 0, 0, time.UTC), "Bill Deer"},
-				"Hi Bill Deer, it is your birthday on 1934-05-01T00:00:00Z",
-				171,
-			},
-			{sendingReport{"Second Report", 20},
-				`Your "Second Report" report is ready. You've sent 20 messages.`,
-				186,
-			},
+			{fullTime{name: "Alice", salary: 10000}, 10000},
+			{contractor{name: "John", hourlyPay: 1000, hoursPerYear: 1000}, 1000000},
 		}...)
 	}
 
@@ -39,22 +25,22 @@ func Test(t *testing.T) {
 	failCount := 0
 
 	for _, test := range tests {
-		text, cost := sendMessage(test.msg)
-		if text != test.expectedText || cost != test.expectedCost {
+		salary := test.emp.getSalary()
+		if salary != test.expected {
 			failCount++
 			t.Errorf(`---------------------------------
 Inputs:     %+v
-Expecting:  (%v, %v)
-Actual:     (%v, %v)
-Fail`, test.msg, test.expectedText, test.expectedCost, text, cost)
+Expecting:  %v
+Actual:     %v
+Fail`, test.emp, test.expected, salary)
 		} else {
 			passCount++
 			fmt.Printf(`---------------------------------
 Inputs:     %+v
-Expecting:  (%v, %v)
-Actual:     (%v, %v)
+Expecting:  %v
+Actual:     %v
 Pass
-`, test.msg, test.expectedText, test.expectedCost, text, cost)
+`, test.emp, test.expected, salary)
 		}
 	}
 
