46abe551cd93f57d2d6049ca84e5a834c1a74156
Yer01 <enunaaaa.zhekenov@gmail.com>
2025-01-26 22:14:18 +0500

    Type Assertion

--- sample-app/main.go
+++ sample-app/main.go
@@ -1,30 +1,49 @@
 package main
 
-import "fmt"
+func getExpenseReport(e expense) (string, float64) {
 
-func (e email) cost() int {
-	if e.isSubscribed {
-		return len(e.body) * 2
+	if m, ok := e.(email); ok {
+		return m.toAddress, m.cost()
+	} else if m, ok := e.(sms); ok {
+		return m.toPhoneNumber, m.cost()
 	}
-	return len(e.body) * 5
+	return "", 0.0
 }
 
-func (e email) format() string {
-	if !e.isSubscribed {
-		return fmt.Sprintf("'%s' | Not Subscribed", e.body)
-	}
-	return fmt.Sprintf("'%s' | Subscribed", e.body)
-}
+// don't touch below this line
 
 type expense interface {
-	cost() int
-}
-
-type formatter interface {
-	format() string
+	cost() float64
 }
 
 type email struct {
 	isSubscribed bool
 	body         string
+	toAddress    string
+}
+
+type sms struct {
+	isSubscribed  bool
+	body          string
+	toPhoneNumber string
+}
+
+type invalid struct{}
+
+func (e email) cost() float64 {
+	if !e.isSubscribed {
+		return float64(len(e.body)) * .05
+	}
+	return float64(len(e.body)) * .01
+}
+
+func (s sms) cost() float64 {
+	if !s.isSubscribed {
+		return float64(len(s.body)) * .1
+	}
+	return float64(len(s.body)) * .03
+}
+
+func (i invalid) cost() float64 {
+	return 0.0
 }
