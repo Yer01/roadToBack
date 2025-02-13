60f2664811cf1adcd81b5823f14423f92cb22f5b
Yer01 <enunaaaa.zhekenov@gmail.com>
2025-02-14 01:06:01 +0500

    Mutations

---
+++ sample-app/mutations.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func deleteIfNecessary(users map[string]user, name string) (deleted bool, err error) {
+	elem, ok := users[name]
+	if !ok {
+		return false, fmt.Errorf("not found")
+	}
+	if elem.scheduledForDeletion {
+		delete(users, name)
+		return true, nil
+	} else {
+		return false, nil
+	}
+
+}
+
+type user struct {
+	name                 string
+	number               int
+	scheduledForDeletion bool
+}
